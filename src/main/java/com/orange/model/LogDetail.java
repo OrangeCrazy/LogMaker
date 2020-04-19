128d61f9aa5f1b8378b939269f4af5358dca84c5
OrangeCrazy <zczycz@live.com>
2020-04-19 17:11:56 +0800

    ReadMe文档更新
    Signed-off-by: OrangeCrazy <zczycz@live.com>

---
+++ src/main/java/com/orange/model/LogDetail.java
@@ -0,0 +1,18 @@
+package com.orange.model;
+
+import lombok.Data;
+
+/**
+ * @author OrangeCrazy
+ * @version 1.0
+ * @date 2020/4/19 17:10
+ */
+@Data
+public class LogDetail {
+
+    /**
+     * 暂缺
+     *
+     */
+
+}
